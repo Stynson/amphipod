5cc83c32960f0e0e7cef4d75171e8f252d7032d1
Balint Kiss <balint.kiss@shapr3d.com>
2021-12-23 09:29:52 +0100

    adding basic game mechanics

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import Game from "./Game";
+import styled from "styled-components";
+
+import "./App.css";
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #0f0f23;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 function App() {
+  let [input, setInput] = useState<string>("");
+  let ref = useRef<any>();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      {input === "" && (
+        <InputContainer>
+          <textarea
+            ref={ref}
+            style={{ marginBottom: "10px" }}
+            name="Text1"
+            cols={11}
+            rows={5}
+          ></textarea>
+          <button
+            onClick={() => {
+              setInput(ref?.current?.value);
+            }}
+          >
+            Submit input
+          </button>
+        </InputContainer>
+      )}
+      {input && <Game input={input} />}
+    </AppContainer>
   );
 }
 
---
+++ src/Game.tsx
@@ -0,0 +1,205 @@
+import { useEffect, useState } from "react";
+import styled, { css } from "styled-components";
+
+const GameContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const ScoreCard = styled.div`
+  background: #10101a;
+  font-weight: 900;
+  padding: 10px;
+  font-size: 20pt;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  font-family: "Source Code Pro", monospace;
+
+  color: #cccccc;
+  border: 1px solid #333340;
+  margin-bottom: 10px;
+`;
+
+const MapContainer = styled.div`
+  font-family: "Source Code Pro", monospace;
+
+  color: #cccccc;
+  border: 1px solid #333340;
+  font-size: 14pt;
+  background: #10101a;
+  font-weight: 900;
+  padding: 5px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  cursor: pointer;
+`;
+
+const GameRow = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const MapElement = styled.div`
+  width: 12px;
+  height: 23px;
+`;
+
+const PlayableMapElement = styled.div<{ isSelected: boolean }>`
+  width: 12px;
+  height: 23px;
+  &: hover {
+    color: black;
+    background: yellow;
+  }
+  ${(props) =>
+    props.isSelected &&
+    css`
+      background: green;
+    `}
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  margin: 20px;
+`;
+
+const scoreBoard = Object.freeze({
+  A: 1,
+  B: 10,
+  C: 100,
+  D: 1000,
+});
+
+export default function Game({ input }: { input: string }) {
+  let [map, setMap] = useState<Array<Array<string>>>([
+    ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"],
+    ["#", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "#"],
+    ["#", "#", "#", "B", "#", "A", "#", "C", "#", "D", "#", "#", "#"],
+    [" ", " ", "#", "B", "#", "A", "#", "C", "#", "D", "#", " ", " "],
+    [" ", " ", "#", "#", "#", "#", "#", "#", "#", "#", "#", " ", " "],
+  ]);
+
+  let part2 = [
+    [" ", " ", "#", "D", "#", "C", "#", "B", "#", "A", "#", " ", " "],
+    [" ", " ", "#", "D", "#", "B", "#", "A", "#", "C", "#", " ", " "],
+  ];
+  useEffect(() => {
+    setMap((oldMap) => {
+      let copy = JSON.parse(JSON.stringify(oldMap));
+      let [first, second] = input.split("\n").slice(2, 4);
+      console.log(first, second);
+      copy[2] = first.split("");
+      copy[3] = [...second.split(""), " ", " "];
+
+      return copy;
+    });
+  }, [input]);
+
+  let [isPart1, setIsPart1] = useState<any>(null);
+  let [selected, setSelected] = useState<any>(null);
+  let [error] = useState<any>(null);
+  let [score, setScore] = useState<number>(0);
+
+  return (
+    <GameContainer>
+      <ButtonContainer>
+        <button
+          onClick={() => {
+            setIsPart1(true);
+            if (!isPart1) {
+              setMap((oldMap) => {
+                let copy = JSON.parse(JSON.stringify(oldMap));
+                copy.splice(3, 2);
+
+                return copy;
+              });
+            }
+          }}
+        >
+          part1
+        </button>
+        <button
+          onClick={() => {
+            setIsPart1(false);
+            if (isPart1 === true || isPart1 === null) {
+              setMap((oldMap) => {
+                let copy = JSON.parse(JSON.stringify(oldMap));
+                copy.splice(3, 0, part2[0]);
+                copy.splice(4, 0, part2[1]);
+
+                return copy;
+              });
+            }
+          }}
+        >
+          part2
+        </button>
+      </ButtonContainer>
+      <ScoreCard>{score}</ScoreCard>
+      <MapContainer>
+        <div>{error}</div>
+        {map.map((row, i) => (
+          <GameRow>
+            {row.map((c, j) => {
+              if (c !== "#" && c !== " ") {
+                return (
+                  <PlayableMapElement
+                    isSelected={selected?.[0] === i && selected?.[1] === j}
+                    onClick={() => {
+                      if (selected === null) {
+                        if (map[i][j] === ".") {
+                          //setError("Can't select empty");
+                        } else {
+                          setSelected([i, j]);
+                        }
+                      } else {
+                        if (selected[0] === i && selected[1] === j) {
+                          setSelected(null);
+                        } else {
+                          if (map[i][j] !== ".") {
+                            //  setError("Can't step on another");
+                          } else {
+                            const [si, sj] = selected;
+                            setScore((score) => {
+                              let moveCount =
+                                i - 1 + Math.abs(sj - j) + (si - 1);
+                              return (
+                                score + moveCount * scoreBoard[map[si][sj]]
+                              );
+                            });
+                            setMap((oldMap) => {
+                              let copy = JSON.parse(JSON.stringify(oldMap));
+                              copy[i][j] = copy[si][sj];
+                              copy[si][sj] = ".";
+                              return copy;
+                            });
+                            setSelected(null);
+                          }
+                        }
+                      }
+                    }}
+                  >
+                    {c}
+                  </PlayableMapElement>
+                );
+              } else {
+                return <MapElement>{c}</MapElement>;
+              }
+            })}
+          </GameRow>
+        ))}
+      </MapContainer>
+    </GameContainer>
+  );
+}
+
+//let foo =  `#...........#
+//</div> ###B#C#B#D###
+//</div>   #A#D#C#A#
+//</div>   #A#D#C#A#
+//</div>   #A#D#C#A#
+//</div>   #########`
